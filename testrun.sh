4ce2ce83e572bd558e341023768a66462ca9351c
Custer Cameron <s101061877@george.cse.sdsmt.edu>
2021-03-21 13:22:25 -0600

    testrun modified for compiling with correct flags

--- testrun.sh
+++ testrun.sh
@@ -11,13 +11,13 @@ fi
 g++ generate.C -o generate.out
 if [ "$sort" = "heapsort" ]
 then
-	g++ maxheap.C maxheap.S -o heap.out
+	g++ -g -Wall maxheap.C maxheap.S -o heap.out
 	./generate.out > heaptest.in
 	./heap.out
 	rm heaptest.in heap.out
 elif [ "$sort" = "quicksort" ]
 then
-	g++ quicksort.C quicksort.S -o quicksort.out
+	g++ -g -Wall quicksort.C quicksort.S -o quicksort.out
 	./generate.out > quicksort.in
 	./quicksort.out
 	rm quicksort.in quicksort.out
